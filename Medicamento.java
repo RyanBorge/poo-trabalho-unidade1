3e23afc4254e74eed3dcd76532483a868e1bdeac
RyanBorge <143835023+RyanBorge@users.noreply.github.com>
2026-06-02 14:19:19 -0300

    Adiciona a classe Medicamento com atributos e construtor

---
+++ Medicamento.java
@@ -0,0 +1,12 @@
+public class Medicamento {
+    private String nome;
+    private int quantidadeEstoque;
+    private quantidadeMinima;
+
+    public Medicamento(String nome, int quantidadeEstoque, int quantidadeMinima) {
+        this.nome = nome;
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.quantidadeMinima = quantidadeMinima;
+    }
+
+}
